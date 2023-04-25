3104986170507f4f82bd79ba008f98de0ff04897
karanimaina <karani.maina2010@gmail.com>
2023-04-25 23:48:07 +0300

    initialize project

--- src/main/java/com/example/abalytics/config/AppSecurity.java
+++ src/main/java/com/example/abalytics/config/AppSecurity.java
@@ -4,9 +4,7 @@ package com.example.abalytics.config;
 @ComponentScan(basePackages={"com.litemore.z_analytics_v2.security"})
 public class AppSecurity extends  WebSecurity {
     @Autowired
-
     private UserDetailsService userDetailsService;
-
     @Autowired
     private RestUnauthorizedEntryPoint entryPoint
 
