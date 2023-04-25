a79e39714ae2c0ae613162767df66c751be0397b
karanimaina <karani.maina2010@gmail.com>
2023-04-25 23:36:34 +0300

    initialize project

--- src/main/java/com/example/abalytics/config/AppSecurity.java
+++ src/main/java/com/example/abalytics/config/AppSecurity.java
@@ -1,6 +1,9 @@
 package com.example.abalytics.config;
 @EnableWebSecurity
 @Configuration
-@ComponentScan(basePackages)
-public class AppSecurity {
+@ComponentScan(basePackages={"com.litemore.z_analytics_v2.security"})
+public class AppSecurity extends  WebSecurity {
+    @Autowired
+    private UserDetailsService
+
 }
